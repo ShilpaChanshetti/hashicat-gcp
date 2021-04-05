4bbd6b209bb6e3e781393cff507c2830efffe7e2
Shilpa <shilpa.chanshetti@tamu.edu>
2021-04-05 18:55:59 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,13 @@
+module "network" {
+  source  = "app.terraform.io/SHILPA-training/network/google"
+  version = "2.5.0"
+  network_name = "shilpa-network"
+  project_id = var.project_id
+  subnets = [
+  {
+    subnet_name   = "shilpa-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
